df0c104ea83f869e87cc8d1a428dd34a620f2606
Jasmine Ryon <jryon12@priorypride.net>
2021-03-19 16:29:36 -0700

    commit tree 5af5057c50c503d21183e0fdba45c3283fded6dd

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,27 @@
 # Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+# url = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+
+class GetRequester
+    attr_accessor :url
+
+    def initialize(url)   
+        @url = url
+    end
+
+    def get_response_body
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        JSON.parse(response.body)
+    end
+end
+
